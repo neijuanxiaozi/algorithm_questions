4c4de6b9a7844f6a4674532c255a20bf5c9fe057
jianglong <jianglong.heu@foxmail.com>
2024-07-01 08:26:03 +0800

    图论添加课程表I和II两道题

--- 7_图论.cpp
+++ 7_图论.cpp
@@ -208,40 +208,41 @@ private:
 class Solution {
 private:
     vector<vector<int>> edges;
-    //每个节点有三个状态 未搜索 搜索中 已完成 对用0 1 2
-    vector<int> visited;
-    bool valid = true;
+    vector<int> indeg;
+
 public:
-    void dfs(int u) {
-        visited[u] = 1;
-        for (int v : edges[u]) {
-            if (visited[v] == 0) {
-                dfs(v);
-                if (!valid) {
-                    return;
-                }
-            }
-            else if (visited[v] == 1) {
-                valid = false;
-                return;
-            }
-        }
-        visited[u] = 2;
-    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         edges.resize(numCourses);
-        visited.resize(numCourses);
-        for (const auto &info : prerequisites) {
+        indeg.resize(numCourses);
+        for (const auto& info: prerequisites) {
             edges[info[1]].push_back(info[0]);
+            ++indeg[info[0]];
         }
-        for (int i = 0; i < numCourses && valid; ++i) {
-            if (!visited[i]) {
-                dfs(i);
+
+        queue<int> q;
+        for (int i = 0; i < numCourses; ++i) {
+            if (indeg[i] == 0) {
+                q.push(i);
             }
         }
-        return valid;
+
+        int visited = 0;
+        while (!q.empty()) {
+            ++visited;
+            int u = q.front();
+            q.pop();
+            for (int v: edges[u]) {
+                --indeg[v];
+                if (indeg[v] == 0) {
+                    q.push(v);
+                }
+            }
+        }
+
+        return visited == numCourses;
     }
 };
+
 
 //深度优先遍历
 class Solution {
@@ -285,5 +286,113 @@ public:
             }
         }
         return valid;
+    }
+};
+
+/*
+210. 课程表II 与课程表I一样 只是需要记录答案 https://leetcode.cn/problems/course-schedule-ii/description/?envType=study-plan-v2&envId=top-interview-150
+
+*/
+
+//广度
+class Solution {
+private:
+    vector<vector<int>> edges;
+    vector<int> indeg;
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        edges.resize(numCourses);
+        indeg.resize(numCourses);
+        vector<int> res;//新增
+
+        for (auto &item : prerequisites) {
+            edges[item[1]].push_back(item[0]);
+            ++indeg[item[0]];
+        }
+
+        queue<int> que;
+        for (int i = 0; i < indeg.size(); ++i) {
+            if (indeg[i] == 0) {
+                que.push(i);
+            }
+        }
+
+        int visited = 0;
+        while (!que.empty()) {
+            ++visited;
+            int temp = que.front();
+            que.pop();
+            res.push_back(temp);//新增
+            for (auto &item : edges[temp]) {
+                --indeg[item];
+                if (indeg[item] == 0) {
+                    que.push(item);
+                }
+            }
+        }
+        if (visited == numCourses) {//新增
+            return res;//新增
+        }//新增
+        return {};
+    }
+};
+
+//深度
+class Solution {
+private:
+    // 存储有向图
+    vector<vector<int>> edges;
+    // 标记每个节点的状态：0=未搜索，1=搜索中，2=已完成
+    vector<int> visited;
+    // 用数组来模拟栈，下标 0 为栈底，n-1 为栈顶
+    vector<int> result;
+    // 判断有向图中是否有环
+    bool valid = true;
+
+public:
+    void dfs(int u) {
+        // 将节点标记为「搜索中」
+        visited[u] = 1;
+        // 搜索其相邻节点
+        // 只要发现有环，立刻停止搜索
+        for (int v: edges[u]) {
+            // 如果「未搜索」那么搜索相邻节点
+            if (visited[v] == 0) {
+                dfs(v);
+                if (!valid) {
+                    return;
+                }
+            }
+            // 如果「搜索中」说明找到了环
+            else if (visited[v] == 1) {
+                valid = false;
+                return;
+            }
+        }
+        // 将节点标记为「已完成」
+        visited[u] = 2;
+        // 将节点入栈
+        result.push_back(u);
+    }
+
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        edges.resize(numCourses);
+        visited.resize(numCourses);
+        for (const auto& info: prerequisites) {
+            edges[info[1]].push_back(info[0]);
+        }
+        // 每次挑选一个「未搜索」的节点，开始进行深度优先搜索
+        for (int i = 0; i < numCourses && valid; ++i) {
+            if (!visited[i]) {
+                dfs(i);
+            }
+        }
+        if (!valid) {
+            return {};
+        }
+        // 如果没有环，那么就有拓扑排序
+        // 注意下标 0 为栈底，因此需要将数组反序输出
+        reverse(result.begin(), result.end());
+        return result;
     }
 };
