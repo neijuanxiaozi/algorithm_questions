dd1db68594be6975b7602abf5286d657edeed497
jianglong <jianglong.heu@foxmail.com>
2024-07-11 08:42:42 +0800

    图论新增拓扑排序

--- 7_图论.cpp
+++ 7_图论.cpp
@@ -353,9 +353,67 @@ int main() {
     return 0;
 }
 
+/**
+拓扑排序精讲
+题目描述：
+    某个大型软件项目的构建系统拥有 N 个文件，文件编号从 0 到 N - 1，在这些文件中，某些文件依赖于其他文件的内容，这意味着如果文件 A 依赖于文件 B，则必须在处理文件 A 之前处理文件 B （0 <= A, B <= N - 1）。请编写一个算法，用于确定文件处理的顺序。
+
+输入描述：
+    第一行输入两个正整数 M, N。表示 N 个文件之间拥有 M 条依赖关系。
+    后续 M 行，每行两个正整数 S 和 T，表示 T 文件依赖于 S 文件。
+输出描述：
+    输出共一行，如果能处理成功，则输出文件顺序，用空格隔开。
+    如果不能成功处理（相互依赖），则输出 -1。
+*/
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+using namespace std;
+int main() {
+    int m, n, s, t;
+    cin >> n >> m;
+    vector<int> inDegree(n, 0); // 记录每个文件的入度
+
+    unordered_map<int, vector<int>> umap;// 记录文件依赖关系
+    vector<int> result; // 记录结果
+
+    while (m--) {
+        // s->t，先有s才能有t
+        cin >> s >> t;
+        inDegree[t]++; // t的入度加一
+        umap[s].push_back(t); // 记录s指向哪些文件
+    }
+    queue<int> que;
+    for (int i = 0; i < n; i++) {
+        // 入度为0的文件，可以作为开头，先加入队列
+        if (inDegree[i] == 0) que.push(i);
+        //cout << inDegree[i] << endl;
+    }
+    // int count = 0;
+    while (que.size()) {
+        int  cur = que.front(); // 当前选中的文件
+        que.pop();
+        //count++;
+        result.push_back(cur);
+        vector<int> files = umap[cur]; //获取该文件指向的文件
+        if (files.size()) { // cur有后续文件
+            for (int i = 0; i < files.size(); i++) {
+                inDegree[files[i]] --; // cur的指向的文件入度-1
+                if(inDegree[files[i]] == 0) que.push(files[i]);
+            }
+        }
+    }
+    if (result.size() == n) {
+        for (int i = 0; i < n - 1; i++) cout << result[i] << " ";
+        cout << result[n - 1];
+    } else cout << -1 << endl;
+}
+
 /*
 207. 课程表 https://leetcode.cn/problems/course-schedule/description/?envType=study-plan-v2&envId=top-interview-150
-
+    这题也是拓扑排序
     你这个学期必须选修 numCourses 门课程，记为 0 到 numCourses - 1 。
     在选修某些课程之前需要一些先修课程。 先修课程按数组 prerequisites 给出，
     其中 prerequisites[i] = [ai, bi] ，表示如果要学习课程 ai 则 必须 先学习课程  bi 。
