1931a99dd352addaf010848114fed4238c8a1979
jianglong <jianglong.heu@foxmail.com>
2024-07-23 08:44:24 +0800

    图论更新SPFA算法

--- 7_图论.cpp
+++ 7_图论.cpp
@@ -772,7 +772,10 @@ Bellman_ford 是可以计算负权值的单源最短路算法。
     如果能够从城市 1 到连通到城市 n， 请输出一个整数，表示运输成本。
     如果该整数是负数，则表示实现了盈利。如果从城市 1 没有路径可达城市 n，请输出 "unconnected"。
 */
-
+/**
+ * 时间复杂度： O(N * E) , N为节点数量，E为图中边的数量空间复杂度： O(N) ，即 minDist 数组所开辟的空间
+ */
+//主要看下面队列优化的SPFA
 #include <iostream>
 #include <vector>
 #include <list>
@@ -812,4 +815,68 @@ int main() {
     if (minDist[end] == INT_MAX) cout << "unconnected" << endl; // 不能到达终点
     else cout << minDist[end] << endl; // 到达终点最短路径
 
+}
+
+/*
+Bellman_ford队列优化算法(又名SPFA)
+    Bellman_ford 算法每次松弛 都是对所有边进行松弛。但真正有效的松弛，是基于已经计算过的节点在做的松弛。
+    所以 Bellman_ford 算法 每次都是对所有边进行松弛，其实是多做了一些无用功。
+    只需要对 上一次松弛的时候更新过的节点作为出发节点所连接的边 进行松弛就够了。
+    用队列来记录。（其实用栈也行，对元素顺序没有要求）
+*/
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <list>
+#include <climits>
+using namespace std;
+
+struct Edge { //邻接表
+    int to;  // 链接的节点
+    int val; // 边的权重
+
+    Edge(int t, int w): to(t), val(w) {}  // 构造函数
+};
+
+
+int main() {
+    int n, m, p1, p2, val;
+    cin >> n >> m;
+
+    vector<list<Edge>> grid(n + 1); // 邻接表
+
+    // 将所有边保存起来
+    for(int i = 0; i < m; i++){
+        cin >> p1 >> p2 >> val;
+        // p1 指向 p2，权值为 val
+        grid[p1].push_back(Edge(p2, val));
+    }
+    int start = 1;  // 起点
+    int end = n;    // 终点
+
+    vector<int> minDist(n + 1 , INT_MAX);
+    minDist[start] = 0;
+
+    queue<int> que;
+    que.push(start); // 队列里放入起点
+
+    while (!que.empty()) {
+
+        int node = que.front(); que.pop();
+
+        for (Edge edge : grid[node]) {
+            int from = node;
+            int to = edge.to;
+            int value = edge.val;
+            if (minDist[to] > minDist[from] + value) { // 开始松弛
+                minDist[to] = minDist[from] + value;
+                que.push(to);
+            }
+        }
+
+    }
+
+    if (minDist[end] == INT_MAX) cout << "unconnected" << endl; // 不能到达终点
+    else cout << minDist[end] << endl; // 到达终点最短路径
 }
